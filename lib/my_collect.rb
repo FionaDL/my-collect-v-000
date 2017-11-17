4f2d1568d21ddf6c8ae3335f5c7061cac53229ab
Fiona Lapham <fionadlapham@gmail.com>
2017-11-17 20:38:33 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,10 +2,8 @@
 def my_collect(array)
   i = 0
   my_collection = []
-  while i < my_collect.length
+  while i < my_collect(array).length
     my_collection << yield(array[x])
         i += 1
   end
 end
-
-my_collect([])
